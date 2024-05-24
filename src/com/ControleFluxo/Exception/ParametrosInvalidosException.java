d3b9e3de4a9c9fc12c02e610a4830e7971a73361
Otávio <oaalves@outlook.com.br>
2024-05-24 00:17:55 -0300

    Desafio logico Realizado. Necessario Doc

--- src/com/ControleFluxo/Exception/ParametrosInvalidosException.java
+++ src/com/ControleFluxo/Exception/ParametrosInvalidosException.java
@@ -1,14 +1,29 @@
 package com.ControleFluxo.Exception;
 
 public class ParametrosInvalidosException extends Exception {
-    
-    boolean verificar(int num1, int num2){
-        boolean verificado = false;
-        if (num1 > num2) {
-            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
-        } else {
-            verificado = true;
+    int codigoErro;
+
+    public ParametrosInvalidosException(int codeError){
+        this.codigoErro = codeError;
+    }
+
+    public void notifyError(){
+        switch (codigoErro) {
+            case 0:
+                System.out.println("Erro encontrado. Não pode ser numero 'Fracionado'.");
+                break;
+            case 1:
+                System.out.println("Erro encontrado. Não pode estar vazio.");
+                break;
+            case 2:
+                System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+                break;
+            case 3:
+                System.out.println("Erro encontrado. Não pode conter Letras.");
+                break;
+            default: System.out.println("Erro não especificado");
+                break;
         }
-        return verificado;
+        
     }
 }
--- src/com/ControleFluxo/main/Contador.java
+++ src/com/ControleFluxo/main/Contador.java
@@ -2,24 +2,70 @@ package com.ControleFluxo.main;
 import com.ControleFluxo.Exception.ParametrosInvalidosException;
 import java.util.Scanner;
 
+FALTA DOCUMENTAR ESSE DESAFIO !!!
+
+
+/** <h1>Contador</h1>
+ * Classe principal contendo interação inicialmente com o usuario atravez da 'import java.util.Scanner'.
+ * <p>
+ * Atuando conferindo 2(Dois) números inteiros e imprimindo por 'sout' (System.out.println()) a incrementação entre partes.
+ * <p>
+ * 
+ */
+
+
 public class Contador {
     
     public static void main(String[] args) {
+        
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o valor: ");
-        int numero1 = input.nextInt();
+        String numero1 = input.nextLine();
         System.out.println("Digite segundo valor: ");
-        int numero2 = input.nextInt();
-
+        String numero2 = input.nextLine();
+        
         try {
+            verificarVariaveis(numero1, numero2);
+        } catch (ParametrosInvalidosException e) {
+           e.notifyError(); 
+        } finally {
+            input.close();
+        }
             
-        } catch (Exception e) {
-            // TODO: handle exception
+    }
+
+/**
+ * Verifica se as entradas do terminal
+ * @param num1
+ * @param num2
+ * @throws ParametrosInvalidosException
+ */
+
+    static void verificarVariaveis(String num1, String num2) throws ParametrosInvalidosException{
+        if (num1.contains(".") || num2.contains(".")) {
+            throw new ParametrosInvalidosException(0);
+        }else if(num1.isEmpty() || num2.isEmpty()){
+            throw new ParametrosInvalidosException(1);
+        }else if(num1.matches(".*[a-zA-Z]+.*") || num2.matches(".*[a-zA-Z]+.*")){
+            throw new ParametrosInvalidosException(3);
+        }else{
+            int numFinal1 = Integer.parseInt(num1);
+            int numFinal2 = Integer.parseInt(num2);
+            boolean verificado = contar(numFinal1, numFinal2);
+            if (verificado == !false) {
+                int incremento = numFinal2 - numFinal1;
+                for (int i = 1; i <= incremento; i++) {
+                    System.out.println("Contando... "+i);
+                }
+            }
         }
     }
 
-    void contar(int num1, int num2) throws ParametrosInvalidosException{
-        throw new ParametrosInvalidosException.verificar(num1,num2);
+    static boolean contar(int num1, int num2) throws ParametrosInvalidosException{
+        if (num1 > num2) {
+            throw new ParametrosInvalidosException(2);
+        }
+        return true;
     }
 }
 
