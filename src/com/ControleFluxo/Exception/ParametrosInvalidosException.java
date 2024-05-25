683e16ddcb36801cb9509c020f0bbf4bec58d47b
Otávio <oaalves@outlook.com.br>
2024-05-25 00:01:10 -0300

    Desafio Finalizado

--- src/com/ControleFluxo/Exception/ParametrosInvalidosException.java
+++ src/com/ControleFluxo/Exception/ParametrosInvalidosException.java
@@ -1,34 +1,37 @@
 package com.ControleFluxo.Exception;
-
+/**
+ * <h1>Parametros Invalidos Exception</h1>
+ * <p>
+ * Exceção customizada, utilizada para verificar qual tipo de Exceção é enviada pelo <b>"try-cacth"</b>
+ * <ul>
+ *  <li>
+ *      Método
+ *  <b>notifyError</b> - Casos verificado e mensagem de exception capturada.
+ * 
+ *   </li>
+ *  <li>
+ *      Contrutor
+ *  <b>ParametrosInvalidosException()</b> - Recepciona qual entrada de valor para memorizar codigoErro
+ *  </li>
+ * </ul>
+ */
 public class ParametrosInvalidosException extends Exception {
     int codigoErro;
     String firstString;
     String secunndString;
 
-    public ParametrosInvalidosException(String firstString, String secunndString){
-        this.firstString = firstString;
-        this.secunndString = secunndString;
+    public ParametrosInvalidosException(int codigoErro){
+        this.codigoErro = codigoErro;
     }
-
-    private void verificarParametro(){
-        if (firstString.contains(".") || secunndString.contains(".")) {
-            codigoErro = 0;
-        }else if(firstString.isEmpty() || secunndString.isEmpty()){
-            codigoErro = 1;
-        }else if(firstString.matches(".*[a-zA-Z]+.*") || secunndString.matches(".*[a-zA-Z]+.*")){
-            codigoErro = 3;
-        }else{
-            int numFinal1 = Integer.parseInt(firstString);
-            int numFinal2 = Integer.parseInt(secunndString);
-        }
-            
-
-    }
-
+    /**
+     * <h3>Metodo <b>notifyError</3></b>
+     * Metodo sem parametro para selecionar o caso verificado.
+     * @param codigoErro Variavel memorizada pelo Contrutor.
+     */
     public void notifyError(){
         switch (codigoErro) {
             case 0:
-                System.out.println("Erro encontrado. Não pode ser numero 'Fracionado'.");
+                System.out.println("Erro encontrado. Não pode ser número 'Fracionado'.");
                 break;
             case 1:
                 System.out.println("Erro encontrado. Não pode estar vazio.");
@@ -39,9 +42,11 @@ public class ParametrosInvalidosException extends Exception {
             case 3:
                 System.out.println("Erro encontrado. Não pode conter Letras.");
                 break;
+            case 4:
+                System.out.println("Erro encontrado. Primeiro Parametro não pode ser maior.");
+                break;
             default: System.out.println("Erro não especificado");
                 break;
         }
-        
     }
 }
--- src/com/ControleFluxo/main/Contador.java
+++ src/com/ControleFluxo/main/Contador.java
@@ -1,26 +1,26 @@
 package com.ControleFluxo.main;
 import com.ControleFluxo.Exception.ParametrosInvalidosException;
 import java.util.Scanner;
-
 /** <h1>Contador</h1>
  * Classe principal contendo interação inicialmente com o usuario atravez da 'import java.util.Scanner'.
  * <p>
  * Atuando conferindo 2(Dois) números inteiros e imprimindo por 'sout' (System.out.println()) a incrementação entre partes.
  * <p>
- * 
  */
-
-
 public class Contador {
-    
+    /**
+     * Metodo Main, principal disparador do codigo. Sem args.
+     * @param input Acionador da classe Scanner, interação com usuário no términal.
+     * @param numero1 String recebi o valor digitado.
+     * @param numero2 String recebe o valor digitado.
+     * @throws ParametrosInvalidosException Exceção disparada em casos de tipos de dados 
+     */
     public static void main(String[] args) {
-        
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o valor: ");
         String numero1 = input.nextLine();
         System.out.println("Digite segundo valor: ");
         String numero2 = input.nextLine();
-        
         try {
             contar(numero1, numero2);
         } catch (ParametrosInvalidosException e) {
@@ -28,22 +28,34 @@ public class Contador {
         } finally {
             input.close();
         }
-            
     }
-
-
-
-    static void contar(String num1, String num2) throws ParametrosInvalidosException{
-            throw new ParametrosInvalidosException(num1,num2);
-        
-           // int incremento = num2 - num1;
-          //  for (int i = 1; i <= incremento; i++) {
-            //    System.out.println("Contando... "+i);
-          //  }
-        
+    /**
+     * Metodo para verificar os valores e empilhar corretamente o exercicio.<p>
+     * Verifica se os valores digitados são na verdade <b>Fracionado, vazio ou letra.</b>
+     * @param firstString   String recebeu do valor digitado
+     * @param secunndString     String recebeu do valor digitado
+     * @throws ParametrosInvalidosException Acionador de throw, cria um disparador de Exceções com codigo de exceção.
+     * @param numFinal1 Quando verdadeiro para valor digitado ser número inteiro, é convertido de String para Int
+     * @param incremento Subtraí as entradas para descobrir a diferença de valores 
+     */
+    static void contar(String firstString, String secunndString) throws ParametrosInvalidosException{
+        if(firstString.contains(".") || secunndString.contains(".")) {
+            throw new ParametrosInvalidosException(0);
+        }else if(firstString.isEmpty() || secunndString.isEmpty()){
+            throw new ParametrosInvalidosException(1);
+        }else if(firstString.matches(".*[a-zA-Z]+.*") || secunndString.matches(".*[a-zA-Z]+.*")){
+            throw new ParametrosInvalidosException(3);
+        }else{
+            int numFinal1 = Integer.parseInt(firstString);
+            int numFinal2 = Integer.parseInt(secunndString);
+            if(numFinal1 < numFinal2) {
+                int incremento = numFinal2 - numFinal1;
+                for(int i = 1; i <= incremento; i++) {
+                    System.out.println("Contando... "+i);
+                }
+            }else{
+                throw new ParametrosInvalidosException(4);
+            }
+        }
     }
-       
 }
-
-
-
